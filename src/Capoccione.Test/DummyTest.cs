8a13366318fbe6f18d2677f79264a0d45f0c8e70
Arialdo Martini <arialdomartini@gmail.com>
2019-10-28 11:50:31 +0100

    To .NET 4.7.2 and .NET Standard 2.0

---
+++ src/Capoccione.Test/DummyTest.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Capoccione.Test
+{
+    public class DummyTest
+    {
+        [Fact]
+        public void dummy_passing_test()
+        {
+            "friends".Should().Be("friends");
+        }
+    }
+}
+
